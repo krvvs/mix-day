84179e561a944ac95e67be856552cacc8db6e2b4
Kris <rewedekaldi@gmail.com>
2023-09-10 00:40:23 +0900

    ADD: Nav UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Image
-        className={styles.logo}
-        src="/icons/all.svg"
-        alt="all icon"
-        width={24}
-        height={24}
-      />
-    </main>
+    <>
+      <Nav />
+    </>
   );
 }
